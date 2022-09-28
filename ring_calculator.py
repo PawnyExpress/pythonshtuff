9c0b09f664bcd6de447fb2be5c4d3480910d1070
David Garcia <davidbhyss@gmail.com>
2022-09-27 20:32:48 -0600

    Updated with conditional statement

--- ring_calculator.py
+++ ring_calculator.py
@@ -4,6 +4,14 @@
 #Spaces and punctuation are counted as engraved units
 
 
+#def cost_of_project(engraving, solid_gold):
+#    cost = solid_gold * (100 + 10 * len(engraving)) + (not solid_gold) * (50 + 7 * len(engraving)) 
+#    return cost
+
+# Same calculator only using a conditional statement
 def cost_of_project(engraving, solid_gold):
-    cost = solid_gold * (100 + 10 * len(engraving)) + (not solid_gold) * (50 + 7 * len(engraving)) 
-    return cost
+    if solid_gold == True:
+        cost = 100 + 10 * len(engraving)
+    else:
+        cost = 50 + 7 * len(engraving)
+    return cost    
