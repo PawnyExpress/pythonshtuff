6f333b9a62b372806759144890922d84833c7018
David Garcia <davidbhyss@gmail.com>
2022-09-27 20:49:26 -0600

    Updated Conditional Examples

--- conditional_statements.py
+++ conditional_statements.py
@@ -23,3 +23,17 @@ def evaluate_temp_with_elif(temp):
     else:
         message = "Low temperature."
     return message
+
+# write a function get_water_bill() that takes as input: num_gallons = the number of gallons of water that a customer used that month. 
+# pricing structure <= 8000 is 5$ a month, <= 22000 is $6, <= 30k is $7 anything above $10 a month.
+
+def get_water_bill(num_gallons):
+    if num_gallons <= 8000:
+        bill = 5 * num_gallons / 1000
+    elif num_gallons <= 22000:
+        bill = 6 * num_gallons / 1000
+    elif num_gallons <= 30000:
+        bill = 7 * num_gallons / 1000
+    else: 
+        bill = 10 * num_gallons / 1000
+    return bill
