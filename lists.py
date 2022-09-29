533882a082255c65d46aae2bf85caa9d398a5291
David Garcia <davidbhyss@gmail.com>
2022-09-28 19:58:45 -0600

    Update to lists

--- lists.py
+++ lists.py
@@ -15,3 +15,31 @@ avg_first_seven = sum(num_customers[:7]) / 7
 avg_last_seven = sum(num_customers[-7:]) / 7 
 max_month = max(num_customers)
 min_month = min(num_customers)
+
+#Create two Python lists:
+
+# DO not change: Define two Python strings
+alphabet = "A.B.C.D.E.F.G.H.I.J.K.L.M.N.O.P.Q.R.S.T.U.V.W.X.Y.Z"
+address = "Mr. H. Potter,The cupboard under the Stairs,4 Privet Drive,Little Whinging,Surrey"
+
+# TODO: Convert strings into Python lists
+letters = alphabet.split(".")
+formatted_address = address.split(",")
+
+
+
+#Use this list comprehension to define a function percentage_liked() that takes one argument as input:
+
+#ratings: list of ratings that people gave to a movie, where each rating is a number between 1-5, inclusive
+#We say someone liked the movie, if they gave a rating of either 4 or 5. Your function should return the percentage of people who liked the movie.
+#For instance, if we supply a value of [1, 2, 3, 4, 5, 4, 5, 1], then 50% (4/8) of the people liked the movie, and the function should return 0.5.
+#Part of the function has already been completed for you. You need only use list_liked to calculate percentage_liked
+
+def percentage_liked(ratings):
+    list_liked = [i>=4 for i in ratings]
+    # TODO: Complete the function
+    percentage_liked = sum(list_liked) / len(list_liked)
+    return percentage_liked
+
+# Do not change: should return 0.5
+percentage_liked([1, 2, 3, 4, 5, 4, 5, 1])
